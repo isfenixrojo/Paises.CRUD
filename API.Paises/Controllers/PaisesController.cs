f385715f6ac580622e9e9f039a24fb59bc3be1d3
Isra <Isra@DESKTOP-K1NLDG6>
2025-03-25 17:45:38 -0600

    Agregar archivos de proyecto.

---
+++ API.Paises/Controllers/PaisesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Paises.Controllers
+{
+    [ApiController]
+    [Route("api/Autores")]
+    public class PaisesController : Controller
+    {
+        [HttpGet]
+        public string GetPaises() {
+            return "Devuelve todos los países";
+
+        }
+
+        [HttpPost]
+        public string PostPais()
+        {
+            return "Inserta un nuevo país";
+
+        }
+
+        [HttpPut]
+        public string  UpdatePaise()
+        {
+            return "Actualiza un registro de un país";
+
+        }
+
+        [HttpDelete]
+        public string DeletePaís()
+        {
+            return "Elimina un registro de un país";
+        }
+        
+    }
+}
