be660688df8bd7982af118f3bc0ed2811fd70fb9
IsraDino <isfenixrojo@gmail.com>
2025-03-25 18:07:09 -0600

    Commit con estructura inicial para configuraciones.

---
+++ API.Paises/Entidades/Ciudad.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Paises.Entidades
+{
+    public class Ciudad
+    {
+        public int Id { get; set; }
+        public string NombrePais { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime Fechaedicion { get; set; }
+
+    }
+}
---
+++ API.Paises/Entidades/Estado.cs
@@ -0,0 +1,6 @@
+﻿namespace API.Paises.Entidades
+{
+    public class Estado
+    {
+    }
+}
